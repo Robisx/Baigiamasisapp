e89ae40720b1f6604d4ec744acda44958b9e5b2e
Robisx <robertxas02@gmail.com>
2017-04-20 08:38:14 +0300

    3 commit

---
+++ app/src/main/java/com/example/user1/baigiamasisapp/MarkerCreationActivity.java
@@ -0,0 +1,13 @@
+package com.example.user1.baigiamasisapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MarkerCreationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marker_creation);
+    }
+}
--- app/src/main/java/com/example/user1/baigiamasisapp/PaskyraActivity.java
+++ app/src/main/java/com/example/user1/baigiamasisapp/PaskyraActivity.java
@@ -1,13 +1,30 @@
 package com.example.user1.baigiamasisapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PaskyraActivity extends AppCompatActivity {
+
+    private Button zymekliai, zemelapis, atsijungti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paskyra);
+
+        zymekliai = (Button) findViewById(R.id.zymekliai);
+        zemelapis = (Button) findViewById(R.id.zemelapis);
+        atsijungti = (Button) findViewById(R.id.atsijungti);
+
+        zemelapis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PaskyraActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
