c3f9c81d3ea0053d2a91514aa23da3a1317caded
Robisx <robertxas02@gmail.com>
2017-04-24 15:06:45 +0300

    4 commit

---
+++ app/src/main/java/com/example/user1/baigiamasisapp/MapTipai.java
@@ -0,0 +1,19 @@
+package com.example.user1.baigiamasisapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by User1 on 2017-04-22.
+ */
+
+public class MapTipai {
+    public static List<String> getTipai(){
+        List<String> result = new ArrayList<>();
+        result.add("Normalus");
+        result.add("Hibridas");
+        result.add("Palydovas");
+        result.add("Paviršiaus");
+        return result;
+    }
+}
--- app/src/main/java/com/example/user1/baigiamasisapp/MapsActivity.java
+++ app/src/main/java/com/example/user1/baigiamasisapp/MapsActivity.java
@@ -2,25 +2,40 @@ package com.example.user1.baigiamasisapp;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
+import com.example.user1.baigiamasisapp.MarkerCreationActivity;
+import com.example.user1.baigiamasisapp.R;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import static com.google.android.gms.maps.GoogleMap.MAP_TYPE_HYBRID;
+import static com.google.android.gms.maps.GoogleMap.MAP_TYPE_NORMAL;
+import static com.google.android.gms.maps.GoogleMap.MAP_TYPE_SATELLITE;
+import static com.google.android.gms.maps.GoogleMap.MAP_TYPE_TERRAIN;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mapas;
+    private Spinner spineris;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.bendras_map);
+        spineris = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, MapTipai.getTipai());
+        spineris.setAdapter(adapter);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -28,15 +43,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mapas = googleMap;
@@ -51,26 +57,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapas.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng latLng) {
-               Intent intent = new Intent(MapsActivity.this, MarkerCreationActivity.class);
+                Intent intent = new Intent(MapsActivity.this, MarkerCreationActivity.class);
                 startActivity(intent);
             }
         });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED
-                || ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            mapas.setMyLocationEnabled(true);
-        }
-    }
 
     private void updateMapType() {
         if (mapas == null) {
             return;
         }
+
+        spineris.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        mapas.setMapType(MAP_TYPE_NORMAL);
+                        break;
+                    case 1:
+                        mapas.setMapType(MAP_TYPE_HYBRID);
+                        break;
+                    case 2:
+                        mapas.setMapType(MAP_TYPE_SATELLITE);
+                        break;
+                    case 3:
+                        mapas.setMapType(MAP_TYPE_TERRAIN);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
